b788aa74219935f80a982ade6b936dcdff1ad757
Kaoutar Ouadih <kaoutar.ouadih9@gmail.com>
2023-11-28 22:44:50 +0100

    add the backend of add new book and manage the dialog element

--- javascript.js
+++ javascript.js
@@ -23,3 +23,45 @@ console.log(myLibrary);
 myLibrary.forEach(element => {
     console.log(element);
 });
+
+const addBtn = document.querySelector('#addBtn');
+const dialog = document.querySelector('dialog');
+const addClose = document.querySelector('#addClose');
+const author = document.querySelector('#author');
+const title = document.querySelector('#title');
+const numPages = document.querySelector('#numPages');
+const read = document.querySelector('#read');
+const output = document.querySelector('#output');
+
+// for outputs
+const authorName = document.createElement('p');
+const bookTitle = document.createElement('p');
+const bookNumPages = document.createElement('p');
+
+
+addBtn.addEventListener("click", ()=>{
+    dialog.showModal();
+})
+
+author.addEventListener('input', ()=>{
+    authorName.textContent = author.value;
+})
+
+title.addEventListener('input', ()=>{
+    bookTitle.textContent = title.value;
+})
+
+numPages.addEventListener('input', ()=>{
+    bookNumPages.textContent = numPages.value;
+})
+
+
+
+
+addClose.addEventListener("click", (event)=>{
+    event.preventDefault();// We don't want to submit this fake form
+    output.appendChild(authorName);
+    output.appendChild(bookTitle);
+    output.appendChild(bookNumPages);
+    dialog.close(authorName);
+})
