e159656631f5a147a02e27623326d1c8bcba5a75
Kaoutar Ouadih <kaoutar.ouadih9@gmail.com>
2023-11-30 20:09:10 +0100

    display the state of a book if it's already read or nt!

--- javascript.js
+++ javascript.js
@@ -12,14 +12,6 @@ function addBookToLibrary(book){
  myLibrary.push(book);
 }
 
-// const Book1 = new Book("James Clear", "Atomic Habits", 314);
-// console.log(Book1.Author);
-// addBookToLibrary(Book1);
-
-// myLibrary.forEach(element => {
-//     console.log(element);
-// });
-
 const addBtn = document.querySelector('#addBtn');
 const dialog = document.querySelector('dialog');
 const addClose = document.querySelector('#addClose');
@@ -52,51 +44,58 @@ numPages.addEventListener('input', ()=>{
     Pages = numPages.value;
 })
 
-read.addEventListener('input', ()=>{
-    readOrNot.textContent = numPages.value;
-})
+// read.addEventListener('input', ()=>{
+//     readOrNot.textContent = numPages.value;
+// })
 
 
 addClose.addEventListener("click", (event)=>{
     event.preventDefault();// We don't want to submit this fake form
     const myBook = new Book(Name, TitleVar, Pages);
-    console.log(myBook);
     addBookToLibrary(myBook);
 
     const authorName = document.createElement('p');
     const bookTitle = document.createElement('p');
     const bookNumPages = document.createElement('p');
-    // const readOrNot = document.createElement('button');
+    const readOrNot = document.createElement('h3');
+    const remove = document.createElement('button');
+
+    //Styling the card and the delete button
     const card = document.createElement('div');
     card.style.backgroundColor = 'white';
     card.style.borderRadius = '8px';
     card.style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.1)';
-    console.log(myBook.Name);
-    authorName.textContent = myBook.Author;
-    bookTitle.textContent = myBook.title;
-    bookNumPages.textContent = myBook.numPages;
+    card.style.padding = '10px';
+    remove.style.backgroundColor = '#3464eb';
+    remove.style.border = 'none';
+    remove.style.borderRadius = '8px';
+    remove.style.height = '35px';
+    remove.style.width = '80%';
 
-    card.append(authorName);
-    card.append(bookTitle);
-    card.append(bookNumPages);
+    
+    authorName.textContent = myBook.Author;
+    bookTitle.textContent = `" ${myBook.title} "`;
+    bookNumPages.textContent = `${myBook.numPages} pages`;
+    remove.textContent = 'Delete';
+
+    card.appendChild(authorName);
+    card.appendChild(bookTitle);
+    card.appendChild(bookNumPages);
+    if(read.checked){
+        console.log("checked");
+        readOrNot.textContent = "read";
+        readOrNot.style.color = "green";
+        card.appendChild(readOrNot);
+    }else{
+        console.log("unchecked");
+        readOrNot.textContent = "not read";
+        readOrNot.style.color = 'red';
+        card.appendChild(readOrNot);
+    }
+    card.appendChild(remove);
 
     container.appendChild(card);
 
     dialog.close();
 })
 
-
-
-
-// const tab = [{name :"kaoutar", age: 200}, {name :"salma", age: 100}];
-// tab.forEach(element => {
-//     console.log(element);
-//     const nameElt = document.createElement('h3');
-//     const ageElt = document.createElement('p');
-//     nameElt.textContent = element.name;
-//     ageElt.textContent = element.age;
-//     container.appendChild(nameElt);
-//     container.appendChild(ageElt);
-
-    
-// });
