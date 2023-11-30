a618b6b245bdb7d5bfac1665defc2a67d0e31d0e
Kaoutar Ouadih <kaoutar.ouadih9@gmail.com>
2023-11-30 18:49:55 +0100

    listing books + and handle dialog return value

--- javascript.js
+++ javascript.js
@@ -1,28 +1,24 @@
 const myLibrary = [];
 
 //the constructor
-function Book(author, title, numPages, read){
+function Book(author, title, numPages){
   this.Author = author;
   this.title = title;
   this.numPages = numPages;
-  this.read = read;
+//   this.read = read;
 }
 
 function addBookToLibrary(book){
  myLibrary.push(book);
 }
 
-const Book1 = new Book("James Clear", "Atomic Habits", 314, "yes");
-console.log(Book1.Author);
-addBookToLibrary(Book1);
-console.log(myLibrary);
-const Book2 = new Book("James Clear", "Atomic Habits", 314, "yes");
-addBookToLibrary(Book1);
-console.log(myLibrary);
+// const Book1 = new Book("James Clear", "Atomic Habits", 314);
+// console.log(Book1.Author);
+// addBookToLibrary(Book1);
 
-myLibrary.forEach(element => {
-    console.log(element);
-});
+// myLibrary.forEach(element => {
+//     console.log(element);
+// });
 
 const addBtn = document.querySelector('#addBtn');
 const dialog = document.querySelector('dialog');
@@ -31,37 +27,76 @@ const author = document.querySelector('#author');
 const title = document.querySelector('#title');
 const numPages = document.querySelector('#numPages');
 const read = document.querySelector('#read');
-const output = document.querySelector('#output');
+const container = document.querySelector('#container');
+
 
 // for outputs
-const authorName = document.createElement('p');
-const bookTitle = document.createElement('p');
-const bookNumPages = document.createElement('p');
+let Name;
+let TitleVar;
+let Pages;
 
 
 addBtn.addEventListener("click", ()=>{
     dialog.showModal();
 })
 
-author.addEventListener('input', ()=>{
-    authorName.textContent = author.value;
+author.addEventListener('input', ()=>{  
+    Name = author.value;
 })
 
 title.addEventListener('input', ()=>{
-    bookTitle.textContent = title.value;
+    TitleVar = title.value;
 })
 
 numPages.addEventListener('input', ()=>{
-    bookNumPages.textContent = numPages.value;
+    Pages = numPages.value;
 })
 
-
+read.addEventListener('input', ()=>{
+    readOrNot.textContent = numPages.value;
+})
 
 
 addClose.addEventListener("click", (event)=>{
     event.preventDefault();// We don't want to submit this fake form
-    output.appendChild(authorName);
-    output.appendChild(bookTitle);
-    output.appendChild(bookNumPages);
-    dialog.close(authorName);
+    const myBook = new Book(Name, TitleVar, Pages);
+    console.log(myBook);
+    addBookToLibrary(myBook);
+
+    const authorName = document.createElement('p');
+    const bookTitle = document.createElement('p');
+    const bookNumPages = document.createElement('p');
+    // const readOrNot = document.createElement('button');
+    const card = document.createElement('div');
+    card.style.backgroundColor = 'white';
+    card.style.borderRadius = '8px';
+    card.style.boxShadow = '3px 3px 5px rgba(0, 0, 0, 0.1)';
+    console.log(myBook.Name);
+    authorName.textContent = myBook.Author;
+    bookTitle.textContent = myBook.title;
+    bookNumPages.textContent = myBook.numPages;
+
+    card.append(authorName);
+    card.append(bookTitle);
+    card.append(bookNumPages);
+
+    container.appendChild(card);
+
+    dialog.close();
 })
+
+
+
+
+// const tab = [{name :"kaoutar", age: 200}, {name :"salma", age: 100}];
+// tab.forEach(element => {
+//     console.log(element);
+//     const nameElt = document.createElement('h3');
+//     const ageElt = document.createElement('p');
+//     nameElt.textContent = element.name;
+//     ageElt.textContent = element.age;
+//     container.appendChild(nameElt);
+//     container.appendChild(ageElt);
+
+    
+// });
